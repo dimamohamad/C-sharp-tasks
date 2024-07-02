80fadb44c103b803d7018081ad00e55ff7bb9a19
dimamohamad <deemaahmad189@gmail.com>
2024-07-02 20:10:00 +0300

    2-7-2024

---
+++ OOP_tasks 2-7-2024/OOP_tasks 2-72-2024/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOP_tasks_2_7_2024
+{
+    internal class Program
+    {
+        //1.	Implement a Car class which has fields to store the car's
+        //make, year, Type, Price, model, Pallet No, and color
+
+        class Car
+        {
+            public string Carmake { get; set; }
+            public int Year { get; set; }
+            public string Type { get; set; }
+            public double Price { get; set; }
+            public string Model { get; set; }
+            public string Palletnumber { get; set; }
+            public string Color { get; set; }
+            public Car(string carmake, int year, string type, double price, string model, string palletnumber, string color)
+            {
+                Carmake = carmake;
+                Year = year;
+                Type = type;
+                Price = price;
+                Model = model;
+                Palletnumber = palletnumber;
+                Color = color;
+            }
+            //2.	Create function name DisplayInfo take two parameters(make, year)
+            public static void DisplayInfo(string carmake, int year)
+            {
+                Console.WriteLine($"the car is {carmake} made in {year}");
+            }
+            //3.	Create function name DisplayInfo take one parameters(make)
+            public static void DisplayInfo(string carmake)
+            {
+                Console.WriteLine($"the car is {carmake}");
+            }
+            //4.	This class contain function name(Display) that print statement(Welcome Car)
+
+            public void Display()
+            {
+                Console.WriteLine("Welcome Car");
+            }
+
+
+
+            //6 – repeat step 4 , 5 with Overriding Concept.
+            //public virtual void Display() {
+            //    Console.WriteLine("Welcome Car");
+            //}
+
+
+        }
+        //5- create a BMW Class  inherit from Car Class
+        //a.This class contain function name(Display) that print statement(welcome BMW)
+        //b-  create object from  BMW then call the function Display
+
+        class BMW : Car
+        {
+            public BMW(string Carmake, int Year, string Type, double Price, string Model, string Palletnumber, string Color)
+            : base(Carmake, Year, Type, Price, Model, Palletnumber, Color)
+            {
+
+
+            }
+            public void Display()
+            {
+                Console.WriteLine("Welcome BMW");
+            }
+
+            //6 – repeat step 4 , 5 with Overriding Concept.
+            //public override void Display()
+            //{
+            //    Console.WriteLine("Welcome BMW");
+            //}
+
+
+
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            
+            BMW bmw = new BMW("BMW", 2022, "Sedan", 41.250, "330i", " 6789XYZ", "white");
+            bmw.Display();
+           Car.DisplayInfo("BMW", 2022);
+           Car.DisplayInfo("Bmw");
+
+        }
+    }
+}
---
+++ OOP_tasks 2-7-2024/task 2 2-7-2024/task 2 2-7-2024/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace task_2_2_7_2024
+{
+    internal class Program
+    {
+        //        1.	Define an Interface
+        //o   Create an interface named ISound that includes a method void MakeSound();.
+
+        public interface ISound
+        {
+
+            void MakeSound();
+
+        }
+        //        2.	Define an Abstract Class
+        //o Create an abstract class named Animal that includes:
+        //	A property string Name { get; set; }.
+        //	A constructor that takes a string name parameter and sets the Name property.
+        //	An abstract method void Eat();.
+        //	A concrete method void Sleep() that prints out the animal's 
+        public abstract class Animal
+        {
+            protected string Name { get; set; }
+            public Animal(string name)
+            {
+                Name = name;
+            }
+            public abstract void Eat();
+            public void Sleep()
+            {
+                Console.WriteLine($"tha animal {Name} is Sleeping ");
+            }
+
+        }
+        //   3.Implement Concrete Classes
+        //o   Create two classes, Dog and Cat, that:
+        //	Inherit from Animal.
+        //	Implement the ISound interface.
+        //	Provide specific implementations for the Eat method that print out the animal's name followed by what it is eating.
+        //	Provide specific implementations for the MakeSound method that print out the animal's name followed by the sound it makes (e.g., "Woof!" for dogs and "Meow!" for cats).
+        public class Dog : Animal, ISound
+        {
+            public Dog(string name) : base(name) { }
+
+            public override void Eat()
+            {
+                Console.WriteLine($"{Name} is eating dog food.");
+            }
+
+            public void MakeSound()
+            {
+                Console.WriteLine($"{Name} says Woof 3aw 3aw 3aw!");
+            }
+        }
+
+        class Cat : Animal, ISound
+        {
+            public Cat(string name) : base(name) { }
+            public override void Eat()
+            {
+                Console.WriteLine($"{Name} is eating cat food.");
+            }
+            public void MakeSound()
+            {
+                Console.WriteLine($"{Name} says Meow meow meow !");
+            }
+
+        }
+        //        4.	Create a Program to Test Your Classes
+        //o   In the Main method, create instances of Dog and Cat.
+        //o Call the Eat, Sleep, and MakeSound methods on each instance.
+
+        static void Main(string[] args)
+        {
+            Dog dog =new Dog("Summer");
+            Cat cat = new Cat("winter");
+            dog.MakeSound();
+            dog.Sleep();
+            dog.Eat();
+            cat.Eat();
+            cat.Sleep();
+            cat.MakeSound();
+
+        }
+    }
+}
